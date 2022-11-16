a44679dcf8d7e4313fb4fcc426a141d1fcee6bef
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-16 16:41:26 +0400

    finish Header: add styles and toggle functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 
 const Header = () => {
+
+// Toggle Menu
+const [isShowMenu, setIsShowMenu] = useState(false)
+
   return (
     <header className='header'>
       <nav className='nav container'>
@@ -9,7 +13,7 @@ const Header = () => {
           Velichko
         </a>
 
-        <div className='nav__menu'>
+        <div className={isShowMenu ? 'nav__menu show-menu' : "nav__menu"}>
           <ul className='nav__list grid'>
             <li className='nav__item'>
               <a href='#home' className='nav__link active-link'>
@@ -53,10 +57,10 @@ const Header = () => {
               </a>
             </li>
           </ul>
-          <i className='uil uil-times nav__close'></i>
+          <i className='uil uil-times nav__close' onClick={() => setIsShowMenu(!isShowMenu)}></i>
         </div>
 
-        <div className='nav__toggle'>
+        <div className='nav__toggle' onClick={() => setIsShowMenu(!isShowMenu)}>
           <i className='uil uil-apps'></i>
         </div>
       </nav>
