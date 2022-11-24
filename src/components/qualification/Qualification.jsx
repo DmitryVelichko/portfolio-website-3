ec65469a9f6894acb2ca436a11323bf1bd487eb7
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-24 23:57:50 +0400

    add Qualification styles

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -15,7 +15,7 @@ const Qualification = () => {
 
       <div className='qualification__container container'>
         <div className='qualification__tabs'>
-          <div className='qualification__button qualification__active button--flex'>
+          <div className={toggleState === 1 ? 'qualification__button qualification__active button--flex' : 'qualification__button button--flex'}>
             <i className='uil uil-graduation-cap qualification__icon'></i>
             Education
           </div>
