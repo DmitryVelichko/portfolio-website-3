49c1dbc484a11a05358831e608a122d8393eff22
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-25 00:00:00 +0400

    add Qualification styles

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,13 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './qualification.css';
 
 const Qualification = () => {
-
-  const [toggleState, setToggleState] = useState(1)
+  const [toggleState, setToggleState] = useState(1);
 
   const toggleTab = (index) => {
-    setToggleState(index)
-  }
+    setToggleState(index);
+  };
   return (
     <section className='qualification section'>
       <h2 className='section__title'>Qualification</h2>
@@ -15,12 +14,24 @@ const Qualification = () => {
 
       <div className='qualification__container container'>
         <div className='qualification__tabs'>
-          <div className={toggleState === 1 ? 'qualification__button qualification__active button--flex' : 'qualification__button button--flex'}>
+          <div
+            className={
+              toggleState === 1
+                ? 'qualification__button qualification__active button--flex'
+                : 'qualification__button button--flex'
+            }
+          >
             <i className='uil uil-graduation-cap qualification__icon'></i>
             Education
           </div>
 
-          <div className={toggleState === 2 ? 'qualification__button qualification__active button--flex' : 'qualification__button button--flex'}>
+          <div
+            className={
+              toggleState === 2
+                ? 'qualification__button qualification__active button--flex'
+                : 'qualification__button button--flex'
+            }
+          >
             <i className='uil uil-briefcase-alt qualification__icon'></i>
             Experience
           </div>
@@ -46,8 +57,8 @@ const Qualification = () => {
             </div>
 
             <div className='qualification__data'>
-                <div></div>
-                <div>
+              <div></div>
+              <div>
                 <span className='qualification__rounder'></span>
                 <span className='qualification__line'></span>
               </div>
@@ -59,7 +70,7 @@ const Qualification = () => {
                 <div className='qualification__calendar'>
                   <i className='uil uil-calendar-alt'></i>2020 - 2021
                 </div>
-              </div>     
+              </div>
             </div>
 
             <div className='qualification__data'>
@@ -80,8 +91,8 @@ const Qualification = () => {
             </div>
 
             <div className='qualification__data'>
-                <div></div>
-                <div>
+              <div></div>
+              <div>
                 <span className='qualification__rounder'></span>
                 <span className='qualification__line'></span>
               </div>
@@ -93,11 +104,8 @@ const Qualification = () => {
                 <div className='qualification__calendar'>
                   <i className='uil uil-calendar-alt'></i>2017 - 2018
                 </div>
-              </div>     
+              </div>
             </div>
-
-
-
           </div>
 
           <div className='qualification__content'>
@@ -119,28 +127,24 @@ const Qualification = () => {
             </div>
 
             <div className='qualification__data'>
-                <div></div>
-                <div>
+              <div></div>
+              <div>
                 <span className='qualification__rounder'></span>
                 <span className='qualification__line'></span>
               </div>
               <div>
                 <h3 className='qualification__title'>UX Designer</h3>
-                <span className='qualification__subtitle'>
-                  Apple - Spain
-                </span>
+                <span className='qualification__subtitle'>Apple - Spain</span>
                 <div className='qualification__calendar'>
                   <i className='uil uil-calendar-alt'></i>2020 - 2021
                 </div>
-              </div>     
+              </div>
             </div>
 
             <div className='qualification__data'>
               <div>
                 <h3 className='qualification__title'>Web Designer</h3>
-                <span className='qualification__subtitle'>
-                  Figma - Spain
-                </span>
+                <span className='qualification__subtitle'>Figma - Spain</span>
                 <div className='qualification__calendar'>
                   <i className='uil uil-calendar-alt'></i>2018 - 2020
                 </div>
