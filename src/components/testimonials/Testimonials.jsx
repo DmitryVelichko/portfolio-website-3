992e5d8dd97c832535b1a147f9f087a3ebc6839a
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-26 00:08:19 +0400

    add Testimonials swiper

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './testimonials.css';
 import { Data } from './Data';
+// Import Swiper React components
+import {Swiper, SwiperSlide} from 'swiper/react'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+// Import required modules
+import {Pagination} from 'swiper'
 
 const Testimonials = () => {
   return (
@@ -8,22 +16,22 @@ const Testimonials = () => {
       <h2 className='section__title'>My clients say</h2>
       <span className='section__subtitle'>Testimonial</span>
 
-      <div className='testimonial__container'>
+      <Swiper className='testimonial__container'>
         {
             Data.map(({id, image, title, description}) => {
                 return (
-                    <div className="testimonial__card" key={id}>
+                    <SwiperSlide className="testimonial__card" key={id}>
                         <img src={image} alt="Human face" 
                         className='testimonial__img'
                         />
 
                         <h3 className="testimonial__name">{title}</h3>
                         <p className="testimonial__description">{description}</p>
-                    </div>
+                    </SwiperSlide>
                 )
             })
         }
-      </div>
+      </Swiper>
     </section>
   );
 };
