501e015c708768c1f049fc3ca03c5b98eb442ee6
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-25 23:24:15 +0400

    add Testimonials styles

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -8,7 +8,22 @@ const Testimonials = () => {
       <h2 className='section__title'>My clients say</h2>
       <span className='section__subtitle'>Testimonial</span>
 
-      <div className='testimonial__container'></div>
+      <div className='testimonial__container'>
+        {
+            Data.map(({id, image, title, description}) => {
+                return (
+                    <div className="testimonial__card" key={id}>
+                        <img src={image} alt="Human face" 
+                        className='testimonial__img'
+                        />
+
+                        <h3 className="testimonial__name">{title}</h3>
+                        <p className="testimonial__description">{description}</p>
+                    </div>
+                )
+            })
+        }
+      </div>
     </section>
   );
 };
