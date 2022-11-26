3cfc106ee4d69befb091d16b743c7c0f4f16f268
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-26 11:22:17 +0400

    add Testimonials styling

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -18,17 +18,15 @@ const Testimonials = () => {
 
       <Swiper
         className='testimonial__container'
-        slidesPerView={1}
-        spaceBetween={10}
+        spaceBetween={24}
+        loop={true}
+        grabCursor={true}
         pagination={{ clickable: true }}
         breakpoints={{
-          640: { slidesPerView: 2, spaceBetween: 20 },
-          768: { slidesPerView: 4, spaceBetween: 40 },
-          1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-          },
+          576: { slidesPerView: 2 },
+          768: { slidesPerView: 2, spaceBetween: 48 },
         }}
+        modules={[Pagination]}
       >
         {Data.map(({ id, image, title, description }) => {
           return (
