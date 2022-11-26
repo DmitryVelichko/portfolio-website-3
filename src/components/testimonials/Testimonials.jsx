b1528c185c1538b9a54d246aa79337705be33084
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-26 10:34:39 +0400

    add Testimonials swiper styles

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './testimonials.css';
 import { Data } from './Data';
 // Import Swiper React components
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
-import 'swiper/css'
-import 'swiper/css/pagination'
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 // Import required modules
-import {Pagination} from 'swiper'
+import { Pagination } from 'swiper';
 
 const Testimonials = () => {
   return (
@@ -16,21 +16,30 @@ const Testimonials = () => {
       <h2 className='section__title'>My clients say</h2>
       <span className='section__subtitle'>Testimonial</span>
 
-      <Swiper className='testimonial__container'>
-        {
-            Data.map(({id, image, title, description}) => {
-                return (
-                    <SwiperSlide className="testimonial__card" key={id}>
-                        <img src={image} alt="Human face" 
-                        className='testimonial__img'
-                        />
+      <Swiper
+        className='testimonial__container'
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: { slidesPerView: 2, spaceBetween: 20 },
+          768: { slidesPerView: 4, spaceBetween: 40 },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+      >
+        {Data.map(({ id, image, title, description }) => {
+          return (
+            <SwiperSlide className='testimonial__card' key={id}>
+              <img src={image} alt='Human face' className='testimonial__img' />
 
-                        <h3 className="testimonial__name">{title}</h3>
-                        <p className="testimonial__description">{description}</p>
-                    </SwiperSlide>
-                )
-            })
-        }
+              <h3 className='testimonial__name'>{title}</h3>
+              <p className='testimonial__description'>{description}</p>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </section>
   );
