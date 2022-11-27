f6133676d6a153180b869a477b0276fe94032bd6
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-28 00:08:19 +0400

    Add Works section

---
+++ src/components/work/Works.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {projectsData} from './Data'
+import { projectsNav } from './Data'
+
+const Works = () => {
+  return (
+   <div className="work__filters">
+    {}
+   </div> 
+  )
+}
+
+export default Works
