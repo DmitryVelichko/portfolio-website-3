d2242aa6486bb0cc9a4035738e3f7f4dcd6427d0
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-27 13:09:25 +0400

    add EmailJs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './contact.css';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_f5l5jin',
+        'template_5exatfi',
+        form.current,
+        'e4Dr4OpDKQmoWy1AI'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
   return (
     <section className='contact section' id='contact'>
       <h2 className='section__title'>Get in Touch</h2>
@@ -55,7 +79,7 @@ const Contact = () => {
         <div className='contact__content'>
           <h3 className='contact__title'>Write me your project</h3>
 
-          <form className='contact__form'>
+          <form ref={form} onSubmit={sendEmail} className='contact__form'>
             <div className='contact__form-div'>
               <label className='contact__form-tag'>Name</label>
               <input
