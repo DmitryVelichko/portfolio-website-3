be9c3b2371866b246b8a9f5b14955dd635f600b5
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-29 14:47:20 +0400

    Add Works section styles

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './work.css'
-import Works from './Works'
+import React from 'react';
+import './work.css';
+import Works from './Works';
 
 const Work = () => {
   return (
-    <section className="work section" id='portfolio'>
-        <h2 className="section__title">Portfolio</h2>
-        <span className="section__subtitle">Most recent works</span>
-
-        <Works />
+    <section className='work section' id='portfolio'>
+      <h2 className='section__title'>Portfolio</h2>
+      <span className='section__subtitle'>Most recent works</span>
+      <Works />
     </section>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
