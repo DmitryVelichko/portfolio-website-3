427253cdff8cf6ad1d00cb75f842d0dab4d85fa1
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-07 19:02:32 +0400

    Fixed home social

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Social = () => {
   return (
-    <div className='home__social'>
+    <div className='home__socialIcons'>
         <a href="https://www.linkedin.com/in/dmitryvelichko/" rel="noreferrer" className='home__social-icon' target='_blank'>
             <i className="uil uil-linkedin"></i>
         </a>
